2b8cf3d78a16b6003bd825b75041f764e5a19aaf
Cammio2611 <cameronwallace94@outlook.com>
2025-05-08 21:09:51 +0100

    WIP backup before cleanup

--- src/backend/notifications.ts
+++
@@ -1,7 +0,0 @@
-// src/backend/notifications.ts
-import { getIO } from "./socket";
-
-export const notifyAdmins = (message: string) => {
-  const io = getIO();
-  io.emit("new_notification", message);
-};
